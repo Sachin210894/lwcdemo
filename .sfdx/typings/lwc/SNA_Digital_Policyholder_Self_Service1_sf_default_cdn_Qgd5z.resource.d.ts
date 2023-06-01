3b4d96c9f539216577163b91c64289fcfdefd446
Harshitautomationn <harshit156@gmail.com>
2023-06-01 14:49:48 +0530

    changes

---
+++ .sfdx/typings/lwc/SNA_Digital_Policyholder_Self_Service1_sf_default_cdn_Qgd5z.resource.d.ts
@@ -0,0 +1,4 @@
+declare module "@salesforce/resourceUrl/SNA_Digital_Policyholder_Self_Service1_sf_default_cdn_Qgd5z" {
+    var SNA_Digital_Policyholder_Self_Service1_sf_default_cdn_Qgd5z: string;
+    export default SNA_Digital_Policyholder_Self_Service1_sf_default_cdn_Qgd5z;
+}
